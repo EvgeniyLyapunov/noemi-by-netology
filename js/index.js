b0059e1c3042f76a05ec06b7f64eed7726a0a84e
EvgeniyLyapunov <lyapunovcs@gmail.com>
2022-10-27 23:13:36 +0300

    work over tablet and desktop media

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // open - close burger menu
   const burgerBtn = document.querySelector(".burger"),
-        menu = document.querySelector(".hero__nav-burger"),
+        menu = document.querySelector(".hero__nav"),
         stickTop = burgerBtn.querySelector(".burger__stick-top"),
         stickMiddle = burgerBtn.querySelector(".burger__stick-middle"),
         stickBottom = burgerBtn.querySelector(".burger__stick-bottom");
 
   burgerBtn.addEventListener("click", () => {
-    menu.classList.toggle("hero__nav-burger_active");
+    menu.classList.toggle("hero__nav_active");
     stickTop.classList.toggle("burger__stick-top_active");
     stickMiddle.classList.toggle("burger__stick-middle_active");
     stickBottom.classList.toggle("burger__stick-bottom_active");
